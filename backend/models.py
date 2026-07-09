6eb8b20d11ad98d5464704359c8195ae9b1e2593
Ansh <nagratnahm@gmail.com>
2026-07-09 12:27:37 +0530

    Add venue tracking and advanced pace spin statistics

--- backend/models.py
+++ backend/models.py
@@ -1,55 +1,68 @@
 from pydantic import BaseModel
+from typing import List
+
 
 class Player(BaseModel):
 
-    # Basic Information
+    # Player Information
 
     name: str
     age: int
     role: str
 
-    # Batting
+
+    # Match Information
 
     matches: int
     innings: int
+    grounds_played: List[str]
+
+
+    # Batting Statistics
+
     runs: int
     balls_faced: int
     fours: int
     sixes: int
     highest_score: int
 
-    # Against Pace
 
-    pace_balls: int
+    # Batting Against Pace
+
+    pace_balls_faced: int
     pace_runs: int
     pace_fours: int
     pace_sixes: int
 
-    # Against Spin
 
-    spin_balls: int
+    # Batting Against Spin
+
+    spin_balls_faced: int
     spin_runs: int
     spin_fours: int
     spin_sixes: int
 
-    # Bowling
 
-    overs: float
+    # Bowling Statistics
+
     balls_bowled: int
     runs_conceded: int
     wickets: int
     maidens: int
+
 
     # Bowling Type
 
     pace_balls_bowled: int
     spin_balls_bowled: int
 
+
     # Bowling Extras
 
     wides: int
     no_balls: int
     dot_balls: int
+
 
     # Fielding
 
